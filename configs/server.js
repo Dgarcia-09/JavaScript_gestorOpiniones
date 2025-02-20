232d016010d47e33593ccf79f6d1af222565ff87
¨Dgarcia-09¨ <dgarcia-2023532@kinal.edu.gt>
2025-02-19 20:40:17 -0600

    feat: edicion basica del usuario anadido

--- configs/server.js
+++ configs/server.js
@@ -4,6 +4,7 @@ import helmet from "helmet"
 import morgan from "morgan"
 import {dbConnection} from "./mongo.js"
 import authRoutes from "../src/auth/auth.routes.js"
+import userRoutes from "../src/user/user.routes.js"
 import apiLimiter from "../src/middlewares/rate-limit-validator.js";
 import {swaggerDocs, swaggerUi} from "./swagger.js"
 
@@ -20,6 +21,7 @@ const middlewares = (app) =>{
 
 const routes = (app) =>{
     app.use("/gestorOpiniones/v1/auth", authRoutes)
+    app.use("/gestorOpiniones/v1/user", userRoutes)
     app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 }
--- src/middlewares/userValidator.js
+++ src/middlewares/userValidator.js
@@ -1,5 +1,5 @@
-import { body } from "express-validator";
-import { emailExists, usernameExists } from "../helpers/db-validator.js";
+import { body, param } from "express-validator";
+import { emailExists, usernameExists, userExists } from "../helpers/db-validator.js";
 import { validarCampos } from "./validate-fields.js";
 import { handleErrors } from "./handle-errors.js";
 
@@ -25,6 +25,21 @@ export const loginValidator = [
     body("email").optional().isEmail().withMessage("No es un email válido"),
     body("username").optional().isString().withMessage("Username es en formáto erróneo"),
     body("password").isLength({min: 4}).withMessage("El password debe contener al menos 8 caracteres"),
+    validarCampos,
+    handleErrors
+]
+
+export const updatePasswordValidator = [
+    param("uid").isMongoId().withMessage("No es un ID válido de MongoDB"),
+    param("uid").custom(userExists),
+    body("newPassword").isLength({min: 8}).withMessage("El password debe contener al menos 8 caracteres"),
+    validarCampos,
+    handleErrors
+]
+
+export const updateUserValidator = [
+    param("uid", "No es un ID válido").isMongoId(),
+    param("uid").custom(userExists),
     validarCampos,
     handleErrors
 ]
